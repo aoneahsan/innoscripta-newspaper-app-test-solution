21b65a984fd1dbb03899c8a46c2b92876600cb78
Ahsan Mahmood <aoneahsan@gmail.com>
2024-06-26 16:06:30 +0500

    vitest tests demo completed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -14,6 +14,10 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: './src/setupTests.ts',
+		coverage: {
+			reporter: ['text', 'html'],
+			provider: 'v8',
+		},
 	},
 	resolve: {
 		alias: {
